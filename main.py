d9cca60f888318b4b91eb9ec0004ed91dd727333
Marvellous-bhp <bhphuocforwork@gmail.com>
2024-06-10 15:13:22 +0700

    update port

--- main.py
+++ main.py
@@ -50,4 +50,5 @@ def hello():
 if __name__ == "__main__":
     # main()
     getThriftBooks()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
