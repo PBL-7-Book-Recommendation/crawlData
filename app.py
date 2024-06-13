3a34874513b22492688191463635618b2eea86a7
MinhTran <nhatminh10b1@gmail.com>
2024-06-13 18:41:32 +0700

    update to deploy

--- app.py
+++ app.py
@@ -7,34 +7,31 @@ import pandas as pd
 from save import saveThriftBooks, saveBookCrossing, saveGoodReads
 from datetime import date
 
+
 def getThriftBooks():
     rawFilePath = crawlThriftBooks.execute()
-    df = preprocessData.executeByAttribute(rawFilePath=rawFilePath, attribute='description', sourceId = 1)
+    df = preprocessData.executeByAttribute(
+        rawFilePath=rawFilePath, attribute="description", sourceId=1
+    )
     print(df)
     inserted_books = saveThriftBooks.execute(df)
     return inserted_books
 
-def getBookCrossingBooks():
-    rawFilePath = r'dataset/book-crossing/raw/1st_book_crossing_data.csv'
-    df = preprocessData.executeByAttribute(rawFilePath=rawFilePath, attribute='description', sourceId = 4)
-    print(df)
 
-    inserted_books = saveBookCrossing.execute(df)
-    return inserted_books
-
-def getGoodReads() :
+def getGoodReads():
     rawFilePath = crawlGoodReads.execute()
-    # rawFilePath = r'D:\Jasmine Data\SuplementarySpaceForOneDrive\PBL7-SRC\crawl-data\goodreads-2024-06-11.csv'
-    df = preprocessData.executeByAttribute(rawFilePath=rawFilePath, attribute='description', sourceId = 3)
+    df = preprocessData.executeByAttribute(
+        rawFilePath=rawFilePath, attribute="description", sourceId=3
+    )
     print(df)
     saveGoodReads.execute(df)
+
 
 def getData():
     if date.today().day != 1:
         return
 
-    getThriftBooks()
-    # getBookCrossingBooks() # dont crawl this periodically
+    # getThriftBooks()
     getGoodReads()
     print("Task executed!")
 
@@ -45,6 +42,7 @@ def main():
     while True:
         schedule.run_pending()
         time.sleep(60)  # sleep for 60s before checking again
+
 
 if __name__ == "__main__":
     main()
--- save/saveGoodReads.py
+++ save/saveGoodReads.py
@@ -1,13 +1,14 @@
 from services import saveService
 from datetime import datetime
 
-source_id = 3 # id for source from GoodReads Books 
+source_id = 3  # id for source from GoodReads Books
+
 
 def convert_timestamp_to_date(timestamp_ms):
     if timestamp_ms is None:
         return None
-        
-    timestamp_s = timestamp_ms / 1000 # Convert milliseconds to seconds
+
+    timestamp_s = timestamp_ms / 1000  # Convert milliseconds to seconds
 
     date_time = datetime.fromtimestamp(timestamp_s)
 
@@ -15,76 +16,81 @@ def convert_timestamp_to_date(timestamp_ms):
 
     return formatted_date
 
+
 def is_valid_date(date_str):
-  try:
-    datetime.strptime(date_str, "%Y-%m-%d") 
-    return True
-  except ValueError:
-    return False
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
 
 def get_number(number_str):
     try:
-        num = int(number_str)  
+        num = int(number_str)
     except ValueError:
-        num = None  
+        num = None
     return num
 
-def insert_book(row, conn, cur):
-    print(f'book = { row.title } ### check_book_existed = {saveService.check_book_existed(row.isbn, cur)}' )
 
-    if not saveService.check_book_existed(row['isbn'], cur):
-        release_date = convert_timestamp_to_date(row.get('publish_date', None)) 
+def insert_book(row, conn, cur):
+    print(
+        f"book = { row.title } ### check_book_existed = {saveService.check_book_existed(row.isbn, cur)}"
+    )
+
+    if not saveService.check_book_existed(row["isbn"], cur):
+        release_date = convert_timestamp_to_date(row.get("publish_date", None))
         if is_valid_date(str(release_date)) == False or release_date is None:
             release_date = None
 
-        number_of_pages = row.get('pages', None)
+        number_of_pages = row.get("pages", None)
         number_of_pages = get_number(number_of_pages)
 
-        book_id = row.get('isbn', None)
+        book_id = row.get("isbn", None)
 
         if book_id:
             query = """
                 INSERT INTO book (
-                    id, title, description, image_url, release_date, 
+                    id, title, description, image_url, release_date,
                     publisher, number_of_pages,
                     source_id, preprocessed_description
                 ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
             book = (
-                str(row['isbn']),
-                row['title'],
-                row['description'],
-                row['image_url'],
+                str(row["isbn"]),
+                row["title"],
+                row["description"],
+                row["image_url"],
                 release_date,
-                row.get('publisher'),
+                row.get("publisher"),
                 number_of_pages,
                 str(source_id),
-                row['preprocessed_description']
+                row["preprocessed_description"],
             )
-            print(book )
+            print(book)
             cur.execute(query, book)
             print(f"Book '{row['title']}' inserted successfully!")
             return row
-        else: 
+        else:
             query = """
                 INSERT INTO book (
-                    title, description, image_url, release_date, 
+                    title, description, image_url, release_date,
                     publisher, number_of_pages,
                     source_id, preprocessed_description
                 ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             """
 
             book = (
-                row['title'],
-                row['description'],
-                row['image_url'],
+                row["title"],
+                row["description"],
+                row["image_url"],
                 release_date,
-                row.get('publisher'),
+                row.get("publisher"),
                 number_of_pages,
                 str(source_id),
-                row['preprocessed_description']
+                row["preprocessed_description"],
             )
-            print(book )
+            print(book)
             cur.execute(query, book)
             print(f"Book '{row['title']}' inserted successfully!")
             return row
@@ -94,10 +100,12 @@ def insert_book(row, conn, cur):
 
 def execute(df):
     all_authors = set()
-    for authors_str in df['author'].dropna():
+    for authors_str in df["author"].dropna():
         if isinstance(authors_str, str):
-            authors_list = authors_str.split(',')
-            all_authors.update([author.strip() for author in authors_list if author.strip() != ''])
+            authors_list = authors_str.split(",")
+            all_authors.update(
+                [author.strip() for author in authors_list if author.strip() != ""]
+            )
 
     # Save authors list
     conn, cur = saveService.connect_db(timeout=30000)
@@ -125,7 +133,7 @@ def execute(df):
     finally:
         cur.close()
         conn.close()
-    
+
     # Save author book relationship
     conn, cur = saveService.connect_db(timeout=3000000)
 
